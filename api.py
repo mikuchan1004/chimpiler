b1b725812b25f74020fa95042050fc6593229af9
김상우 <kimsangwoo22@gmail.com>
2026-09-04 18:10:46 +0900

    api.py : DATABASE_URL 선언 부분 수정

--- api.py
+++ api.py
@@ -2,15 +2,21 @@ from sqlmodel import create_engine, Session # DB 연결 및 테이블 관리를 
 from fastapi import FastAPI, Depends, Request, Form    # FastAPI 기본 기능, 의존성 주입(Depends), 요청 객체(Request), 폼 데이터 처리(Form)
 from fastapi.templating import Jinja2Templates         # HTML 템플릿(Jinja2) 연동을 위한 모듈
 from fastapi.responses import RedirectResponse         # 작업 완료 후 다른 페이지로 이동(리다이렉트)시키는 모듈
-from sqlalchemy import text                           # 원시 SQL(Raw SQL) 문장을 안전하게 실행하기 위한 함수
+from sqlalchemy import text, URL                           # 원시 SQL(Raw SQL) 문장을 안전하게 실행하기 위한 함수
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()  # FastAPI 웹 서버 인스턴스 생성
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory='.')  
 
-DATABASE_URL = 'mysql+pymysql://chimpiler_team:chimpiler!@#@192.168.0.65:3306/chimpiler'
-
+DATABASE_URL = URL.create(
+    drivername = 'mysql+pymysql',
+    username = 'chimplier_team',
+    password='chimpiler!@#',
+    host='192.168.0.65',
+    port='3306',
+    database='chimpiler'
+)
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
@@ -18,11 +24,7 @@ def get_session():
         yield session       # 라우터 함수에 DB 세션을 전달
         session.commit()    # 요청 처리가 무사히 끝나면 변경
 
-###########
-# 페이지 이동
-###########
-
-# 관리자 대시보드 
+# 관리자 대시보드 페이지
 @app.get('/dashboard')
 def list(request: Request, session: Session = Depends(get_session)):
     print('/dashboard 실행')
