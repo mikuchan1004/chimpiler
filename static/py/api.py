c5b6eca3e637658c7c69442aeccdfa199cdb9b87
김상우 <kimsangwoo22@gmail.com>
2026-09-04 17:36:47 +0900

    api.py: 불필요한 SQLModel 임포트 제거 및 공백 정리

--- static/py/api.py
+++ static/py/api.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine, Session, SQLModel  # DB 연결 및 테이블 관리를 위한 SQLModel 모듈
+from sqlmodel import create_engine, Session # DB 연결 및 테이블 관리를 위한 SQLModel 모듈
 from fastapi import FastAPI, Depends, Request, Form    # FastAPI 기본 기능, 의존성 주입(Depends), 요청 객체(Request), 폼 데이터 처리(Form)
 from fastapi.templating import Jinja2Templates         # HTML 템플릿(Jinja2) 연동을 위한 모듈
 from fastapi.responses import RedirectResponse         # 작업 완료 후 다른 페이지로 이동(리다이렉트)시키는 모듈
@@ -16,7 +16,6 @@ def get_session():
         yield session       # 라우터 함수에 DB 세션을 전달
         session.commit()    # 요청 처리가 무사히 끝나면 변경
         
-
 @app.get('/list')
 def list(request: Request, session: Session = Depends(get_session)):
     print('/list 실행')
