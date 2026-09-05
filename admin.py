b817052e30b9c0ae8c3ac0eb6a188e662e9b9315
김상우 <kimsangwoo22@gmail.com>
2026-09-05 11:12:10 +0900

    admin.py : 파일에 주석 추가

--- admin.py
+++ admin.py
@@ -1,8 +1,11 @@
-from sqlmodel import create_engine, Session # DB 연결 및 테이블 관리를 위한 SQLModel 모듈
-from fastapi import FastAPI, Depends, Request, Form    # FastAPI 기본 기능, 의존성 주입(Depends), 요청 객체(Request), 폼 데이터 처리(Form)
-from fastapi.templating import Jinja2Templates         # HTML 템플릿(Jinja2) 연동을 위한 모듈
-from fastapi.responses import RedirectResponse         # 작업 완료 후 다른 페이지로 이동(리다이렉트)시키는 모듈
-from sqlalchemy import text, URL                           # 원시 SQL(Raw SQL) 문장을 안전하게 실행하기 위한 함수
+##########
+# 필요한 모듈 호출 영역
+##########
+from sqlmodel import create_engine, Session
+from fastapi import FastAPI, Depends, Request, Form   
+from fastapi.templating import Jinja2Templates         
+from fastapi.responses import RedirectResponse        
+from sqlalchemy import text, URL                         
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()  # FastAPI 웹 서버 인스턴스 생성
@@ -21,13 +24,12 @@ engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
     with Session(engine) as session: 
-        yield session       # 라우터 함수에 DB 세션을 전달
-        session.commit()    # 요청 처리가 무사히 끝나면 변경
+        yield session     
+        session.commit()   
 
 ##########
 # 페이지 이동 라우터
 ##########
-
 # 관리자 대시보드 페이지
 @app.get('/dashboard')
 def dashboard(request: Request, session: Session = Depends(get_session)):
@@ -69,11 +71,13 @@ def reservations (request:Request, session: Session = Depends(get_session)):
 def main (request:Request, session:Session = Depends(get_session)):
     print('/main 실행')
     return templates.TemplateResponse(request, 'main.html')
-
 ##########
 # 페이지 이동 라우터 끝
 ##########
 
+#########
+# uvicorn 서버 실행 
+#########
 if __name__ == "__main__":
     import uvicorn
     
@@ -83,3 +87,6 @@ if __name__ == "__main__":
     # port=8085: 웹 서버가 사용할 포트 번호 (8000번 대신 지정)
     # reload=True: 코드 변경 시 서버 자동 재시작 (개발용 옵션)
     uvicorn.run("admin:app", port=8085, reload=True, host="0.0.0.0")
+    #########
+    #  uvicorn  서버 실행 끝. 여기가 코드의 마지막입니다.
+    #########
